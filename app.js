e4d025e49c22f047324bcba96addb5e197f0b033
Jorge Gonçalves <u.jorgea@gmail.com>
2020-05-09 20:07:27 +0100

    namespaces with socket

--- app.js
+++ app.js
@@ -1,15 +1,6 @@
-const app = require('express')();
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
-
-server.listen(80);
-// WARNING: app.listen(80) will NOT work here!
-
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
-
+const io = require('socket.io')(80);
 const chat = io.of('/chat').on('connection', (socket) => {
+  // manda para todos. Identico ao broadcast?
   socket.emit('a message', {
     that: 'only',
     '/chat': 'will get',
@@ -22,11 +13,4 @@ const chat = io.of('/chat').on('connection', (socket) => {
 
 const news = io.of('/news').on('connection', (socket) => {
   socket.emit('item', { news: 'item' });
-});
-
-io.on('connection', (socket) => {
-  socket.emit('test', { hello: 'world' });
-  socket.on('my other event', (data) => {
-    console.log(data);
-  });
 });
